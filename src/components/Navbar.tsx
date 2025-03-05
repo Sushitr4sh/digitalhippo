e6bf5b1616955fadf57e71db1d735db47e447eb9
Mario Imanuel Daruranto <mariodaruranto68@gmail.com>
2025-03-05 21:52:37 +0700

    feat: add initial cart component skeleton

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
   const user = null;
@@ -66,7 +67,7 @@ const Navbar = () => {
                   )}
 
                   <div className="ml-4 flow-root lg:ml-6">
-                    {/* Add cart here! */}
+                    <Cart />
                   </div>
                 </div>
               </div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "BDT" | "IDR";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {} //Default value = empty object
+) {
+  const { currency = "USD", notation = "compact" } = options;
+
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat("en-US", {
+    style: "currency", //tells Intl.NumberFormat to format the number as a currency string (e.g., $1,000.00 instead of 1000).
+    currency: currency,
+    notation: notation,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
 }
