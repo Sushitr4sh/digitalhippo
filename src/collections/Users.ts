1e8479668d60ec3105b1c41a0891f2efde0bd8f4
Mario Imanuel Daruranto <mariodaruranto68@gmail.com>
2025-03-09 18:16:06 +0700

    feat(auth): configure Resend transporter for email verification and add verify-email page

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,8 +1,18 @@
+import dotenv from "dotenv";
+import path from "path";
 import { CollectionConfig } from "payload/types";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 export const Users: CollectionConfig = {
   slug: "users",
-  auth: true,
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<a href="${process.env.NEXT_PUBLIC_URL}"/verify-email?token=${token}>Verify Account</a>`;
+      },
+    },
+  },
   access: {
     read: () => true,
     create: () => true,
