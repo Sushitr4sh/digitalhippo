3155e4413c8091f6c6c812410a19eab5a7340afd
Mario Imanuel Daruranto <mariodaruranto68@gmail.com>
2025-03-22 01:43:12 +0700

    feat: add product reel and image slider

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    // Because we're pulling the images from the file system, we need to authorize our local computer to be a valid source for these images
+    remotePatterns: [
+      {
+        hostname: "localhost",
+        pathname: "**",
+        port: "3000",
+        protocol: "http",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
